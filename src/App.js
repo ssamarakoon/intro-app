0dd4c9be3230c113c4a43c22ca9b18ec96a9dcac
ssamarakoon <samitha2005@gmail.com>
2020-07-23 00:33:40 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// congrats on getting this far! your password is the same as your username.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. 
+        </p>
+        <a
+          className="App-link"
+          href="https://vuejs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
